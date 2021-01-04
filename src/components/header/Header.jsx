efe33d4e5edc5c0420d9ee6bba34ffa1e960ff8c
Sheuh Xiong <sheuhxiong@gmail.com>
2021-01-04 11:44:26 -0500

    Removed unused files, image folder, setupTest

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles/header.css";
 import SignOut from "./SignOut";
 
-const Header = (props) => {
+const Header = () => {
   return (
     <div className="header">
       <h1>To Do</h1>
